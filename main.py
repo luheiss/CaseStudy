cc7c5c52b85415d8954bf2e8d9ca4f87223d3e27
luheiss <hl7975@mci4me.at>
2024-12-16 10:23:13 +0100

    change_main

--- main.py
+++ main.py
@@ -1,56 +1,28 @@
 import streamlit as st
-#
-#
-## Initialisiere `authentication_status` im Session State
-#if "authentication_status" not in st.session_state:
-#    st.session_state["authentication_status"] = None
-#if "name" not in st.session_state:
-#    st.session_state["name"] = None
-#
-## Admin-Login-Bereich oben links
-#with st.sidebar:
-#    st.header("Login")
-#    username = st.text_input("Benutzername")
-#    password = st.text_input("Passwort", type="password")
-#    if st.button("Login"):
-#        if username == "admin" and password == "1234":  # Beispiel-Benutzername und Passwort
-#            st.success("Login erfolgreich!")
-#            st.session_state["authentication_status"] = True
-#            st.session_state["name"] = username
-#        else:
-#            st.error("Falscher Benutzername oder Passwort")
-#            st.session_state["authentication_status"] = False
-#
-#tab0 ,tab1, tab2, tab3, tab4 = st.tabs(["🏠Home", "Reservierungen", "Geräte", "Nutzer", "Wartungen"])
-#
-#with tab0:
-#    st.header("Home")
-#    if st.session_state["authentication_status"]:
-#        st.write(f'Welcome *{st.session_state["name"]}*')
-#        st.title('Some content')
-#        if st.button('Logout'):
-#            st.session_state["authentication_status"] = False
-#    else:
-#        st.write("Hallo Gast. Als Gast kannst du nur Geräte reservieren!")
-#with tab1:
-#    st.header("Reserviernungssystem")
-#    #st.write("# Reserviernungssystem")
-#with tab2:
-#    st.header("Geräte- Verwaltung")
-#with tab3:
-#    st.header("Nutzer- Verwaltung")
-#with tab4:
-#    st.header("Wartungsmanagement")
-#
-col1, col2 = st.columns(2)
 
 
-with col1:
-    if st.button("Baloons"):
-        st.balloons()
-with col2:
-   st.write("Test")
+tab1, tab2, tab3, tab4 = st.tabs(["Reservierungen", "Geräte", "Nutzer", "Wartungen"])
 
+
+with tab1:
+    st.header("Reserviernungssystem")
+    #st.write("# Reserviernungssystem")
+with tab2:
+    st.header("Geräte- Verwaltung")
+with tab3:
+    st.header("Nutzer- Verwaltung")
+with tab4:
+    st.header("Wartungsmanagement")
+
+#col1, col2 = st.columns(2)
+#
+#
+#with col1:
+#    if st.button("Baloons"):
+#        st.balloons()
+#with col2:
+#   st.write("Test")
+#
 
 #if st.session_state["authentication_status"]:
 #    authenticator.logout('Logout', 'main')
